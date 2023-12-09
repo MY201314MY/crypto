13e676655d8205f091bdda7f2ed7ba9e5cd98b71
mteaching <3477669308@qq.com>
2023-12-09 10:39:05 +0800

    cryptoauthlib example.

--- src/main.c
+++ src/main.c
@@ -5,9 +5,34 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <cryptoauthlib.h>
+
+#define AES_CONFIG_ENABLE_BIT_MASK 0x01
 
 int main(void)
 {
+	uint8_t revision[4] = {0};
+	ATCA_STATUS status = atcab_info(revision);
+	
+	if(status == ATCA_SUCCESS)
+	{
+		for(uint8_t i=0;i<4;i++)
+		{
+			printf("%02X ", revision[i]);
+		}
+		printf("\r\n");
+	}
+
+	uint8_t aes_enable;
+
+	status = atcab_read_bytes_zone(ATCA_ZONE_CONFIG, 0, 13, &aes_enable, 1);
+	if ((aes_enable & AES_CONFIG_ENABLE_BIT_MASK) == 0)
+	{
+		printf("Ignoring the test, AES is not enabled in config zone");
+	}
+
 	printf("Hello World! %s\n", CONFIG_BOARD);
+
 	return 0;
 }
